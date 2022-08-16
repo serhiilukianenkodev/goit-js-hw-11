d583cd92e89a5fce0d75baa6682721283b35709e
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-08-17 00:10:51 +0300

    intersectionObserver add

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import throttle from 'lodash.throttle';
 
 import {FetchImages} from './js/fetchImages'
 
 const formEl = document.forms["search-form"];
 const galleryEl = document.querySelector('.gallery');
 const loading = document.querySelector('.loading');
-// const loadMoreBtn = document.querySelector('.load-more');
 
 let gallery = new SimpleLightbox('.gallery a', {
 	captions: true,
@@ -20,45 +17,41 @@ let gallery = new SimpleLightbox('.gallery a', {
 	captionDelay: 250,
 	});
 
-// hideEl(loadMoreBtn);
 const fetchImages = new FetchImages()
 
 formEl.addEventListener('submit', onFOrmSubmit)
-// loadMoreBtn.addEventListener('click', onloadMoreClick)
-const throttledLoadMore = throttle(loadMore, 1000)
+
+const infiniteObserver = new IntersectionObserver(([entry], observer) => {
+    if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+        infiniteLoad(); 
+    }
+}, {
+    rootMargin: '100px',
+    threshold: 1.0,
+});
 
 function onFOrmSubmit(evt){
     evt.preventDefault();
-    // hideEl(loadMoreBtn);
     const searchQuery = evt.currentTarget.searchQuery.value
 
     clearGallery();
     
     fetchImages.setQuery(searchQuery);
     fetchImages.getData()
-    .then(data => {
-
-        logMessage(data);
-        return renderGallery(data)})
-    .catch(e => Notify.failure('Sorry, there are no images matching your search query. Please try again.'));
-    
-    window.addEventListener('scroll', throttledLoadMore)
+        .then(data => {
+            logMessage(data);
+            renderGallery(data)})
+        .catch(e => Notify.failure('Sorry, there are no images matching your search query. Please try again.'));
 }
 
-// function onloadMoreClick(){
-//     fetchImages.setNextPage()
-//     fetchImages.getData().then(renderGallery).catch(e => Notify.failure("We're sorry, but you've reached the end of search results."))
-// }
-
 function logMessage(data){
-        const {hits, totalHits} = data;
-    // console.log(hits);
+    const {hits, totalHits} = data;
     if( hits.length === 0) {throw new Error()}
     Notify.success(`Hooay! We found ${totalHits} images.`);
 }
 
 function renderGallery(data){
-
     const {hits} = data;
     const imagesMarkup = hits
     .reduce((markUp,hit) => markUp + createCardMarkup(hit), '')
@@ -66,8 +59,11 @@ function renderGallery(data){
     loading.classList.remove('show');
     galleryEl.insertAdjacentHTML('beforeend', imagesMarkup)
     gallery.refresh()
-    // showEl(loadMoreBtn);
     smoothScrool()
+    //
+    const lastCard = galleryEl.lastElementChild.querySelector('.gallery__image');
+    if (lastCard && !isAllLoad(data)) infiniteObserver.observe(lastCard)
+
 }
 
 function createCardMarkup(data) {
@@ -105,14 +101,6 @@ function clearGallery() {
     galleryEl.innerHTML = '';
 }
 
-// function showEl(elem) {
-//     elem.style.display = '';
-// }
-
-// function hideEl(elem) {
-//     elem.style.display = 'none';
-// }
-
 function smoothScrool(){
     const { height: cardHeight } = document
     .querySelector(".gallery")
@@ -124,28 +112,23 @@ window.scrollBy({
 });
 }
 
-function loadMore(){
-    const galleryHeight = galleryEl.offsetHeight;
-
-    // const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-	// console.log( { scrollTop, scrollHeight, clientHeight });
-    // const need = clientHeight + scrollTop >= scrollHeight - 5;
-    // console.log(need);
-    //
-    
-    const needToDownload = (galleryHeight - scrollY < innerHeight * 2)
-    // console.log(needToDownload);
-
-    if (needToDownload) {
-        loading.classList.add('show');
-        fetchImages.setNextPage()
-        fetchImages.getData().then(renderGallery)
+function infiniteLoad() {
+    loading.classList.add('show');
+    fetchImages.setNextPage();
+    fetchImages.getData()
+        .then(data => { 
+            if (isAllLoad(data)) {
+                Notify.failure("We're sorry, but you've reached the end of search results.")
+                observer.unobserve() 
+            }
+            renderGallery(data)})
         .catch(e => {
-            Notify.failure("We're sorry, but you've reached the end of search results.")
-            window.removeEventListener('scroll', throttledLoadMore)
+            // Notify.failure("Sorry. Something going wrong:(")
             loading.classList.remove('show');
         })
-            
+}
 
-    }
+function isAllLoad(data) {
+    const amount = (fetchImages.getCurrentPage() - 1) * fetchImages.getPerPageAmount() + data.hits.length;
+            return (amount >= data.totalHits) 
 }
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -16,22 +16,21 @@ export class FetchImages{
         this.page += 1;
     }
 
+    getCurrentPage() {
+        return this.page;
+    }
+
+    getPerPageAmount() {
+        return this.perPage;
+    }
 
     async getData(){
         const BASE_URL = 'https://pixabay.com/api/'
         const key = '29142435-196ab0ea47673651fa34d9a29'
         const url = `${BASE_URL}?key=${key}&q=${this.query}s&image_type=photo&per_page=${this.perPage}&page=${this.page}&orientation=horizontal&safesearch=true`
-        // console.log('url', url);
-        
+
         const res = await axios.get(url);
         const data = await res.data;
-        return data;    
-
-
-        // return axios.get(url)
-        // .then(res => {
-        //     console.log('res.data', res.data);
-        //     return res.data
-        // })        
+        return data;         
     }
 }
